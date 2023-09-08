48738dfd328f26408861b4f18b91689639964622
rehaynd hilario <redinx18@gmail.com>
2023-09-08 18:30:43 -0400

    Caso 3: Calcular promedio con dos elementos (Verde)

--- katatdd/conjunto.py
+++ katatdd/conjunto.py
@@ -6,5 +6,7 @@ class Conjunto:
     def promedio(self):
         if len(self.conjunto) == 1:
             return (self.conjunto[0])
+        elif len(self.conjunto) == 2:
+            return (self.conjunto[0] + self.conjunto[1]) / 2    
         else:    
             return None
--- test/test_conjunto.py
+++ test/test_conjunto.py
@@ -6,3 +6,11 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
+
+    def test_conjunto_un_elemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(conjunto.promedio(),5)
+
+    def test_conjunto_dos_elemento(self):
+        conjunto = Conjunto([5,7])
+        self.assertEqual(conjunto.promedio(),6) 
